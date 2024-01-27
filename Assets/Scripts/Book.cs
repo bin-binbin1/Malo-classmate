fd84c11b8985c429a7e200b859420aa66b80e9e1
LMCYI <149228454+LMCYI@users.noreply.github.com>
2024-01-27 22:41:39 +0800

    dropwindow

--- Assets/Scripts/Book.cs
+++ Assets/Scripts/Book.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class Book : MonoBehaviour
 {
@@ -10,9 +11,17 @@ public class Book : MonoBehaviour
     public Sprite pi;
     public int an;
     private Coroutine cor;
+    public GameObject window;
+    List<Vector2> pointList = new List<Vector2>();
+    bool isdrop = false;
+    int num = 0;
     private void Start()
     {
 
+    }
+    private void Update()
+    {
+        
     }
     public void OnTriggerEnter2D(Collider2D collision)
     {
@@ -68,7 +77,7 @@ public class Book : MonoBehaviour
         {
             yield return new WaitForSeconds(1.0f);
             player.SendMessage("angerChange", an);
-            Debug.Log(an);
+            
         }
 
     }
@@ -77,6 +86,42 @@ public class Book : MonoBehaviour
     /// </summary>
     public void dropToWindow()
     {
+            
+        Vector2 MoveSpeed = ( window.transform.position- player.transform.position).normalized * 10;
+        //定义一个列表存放所有的计算的点
+        Debug.Log("movespeed"+MoveSpeed);
+        pointList.Add(player.transform.position);
+        Debug.Log(player.transform.position);
+        for (int i = 1; i < 50; i++)
+        {
+            float time = i * 0.02f * 5;
+            float timePow = time * time;
+            //下一个点
+            Vector2 point = new Vector2(pointList.First().x + MoveSpeed.x * time, pointList.First().y + MoveSpeed.y * time - 0.5f * Physics2D.gravity.magnitude * timePow);
+     
+    
+                pointList.Add(point);//加入到点的列表中
+            
+        }
+        isdrop = true;
+        StartCoroutine(dropwindow());
+    }
 
+    IEnumerator dropwindow()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.1f);
+            if (isdrop)
+            {
+                transform.position = pointList[num];
+                num++;
+                if (num == 20)
+                {
+                    isdrop = false;
+                    Destroy(gameObject);
+                }
+            }
+        }
     }
 }
